e25798c71ccb7040b25e6ea402c2c1b94bd457d1
Roomcar <noteforziio@163.com>
2021-12-30 22:50:06 +0800

    frame: tab

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
   <NavBar />
   <main class="container">
-    <Home />
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link" :class="tab == TABS.annotation ? 'active':''" href="#" @click="tab = TABS.annotation">标注</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" :class="tab == TABS.schema ? 'active':''" href="#" @click="tab = TABS.schema">管理 Schema</a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>-->
+    </ul>
+
+    <Home v-if="tab == TABS.annotation" />
   </main>
 </template>
 
 <script>
+import { ref } from "vue";
 import Home from "@/pages/Home.vue";
 import NavBar from "@/components/NavBar.vue";
+
+const TABS = {
+  annotation: Symbol(),
+  schema: Symbol(),
+}
+
 export default {
   components: { Home, NavBar },
-  name: "App"
+  name: "App",
+  setup() {
+    const tab = ref(TABS.annotation);
+    return { tab, TABS };
+  },
 };
 </script>
