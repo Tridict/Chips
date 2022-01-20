ecd46a62b49bc07f3a62ff825a392c9626f9ba09
Roomcar <noteforziio@163.com>
2022-01-20 13:57:27 +0800

    update path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const resolve = dir => path.join(__dirname, dir);
 module.exports = {
-  publicPath: "/crisps/",
+  publicPath: "/Crisps/",
   assetsDir: "static",
   outputDir: "docs",
   productionSourceMap: false,
